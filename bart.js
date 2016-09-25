7f7e5be3e8652e45124901593d29cb19c39deae6
Willyb15 <william.steen.bryant@gmail.com>
2016-09-24 20:34:30 -0400

    first commit

---
+++ bart.js
@@ -0,0 +1,9 @@
+var javascript = " Javascript is an interpreted scripting langugage that runs in the browser.";
+
+var node = "Node.js lets javascript run outside the browser. This is Node.js i.e. Javascript running outside a browser";
+
+var string = "This is a variable with a string as a value";
+
+console.log(javascript);
+console.log(node);
+console.log(string);
---
+++ will.py
@@ -0,0 +1,11 @@
+python = "This is a program written in python scripting language"
+
+print python
+
+string = "This is a variable with a String as a value"
+
+print string
+
+number = 3
+
+print number
